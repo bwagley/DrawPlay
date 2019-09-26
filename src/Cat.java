46e5561536658ec59bf90baffd82e84a0b74188d
William Hu <whu1@mines.edu>
2019-09-26 10:31:58 -0600

    Made the cat pur by changing the eye size and the mouth size.

--- src/Cat.java
+++ src/Cat.java
@@ -11,10 +11,10 @@ public class Cat {
 	private static final int EYE_X = HEAD_DIMENSION/4;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/2;
 	// pick eye dimensions
-	private static final int EYE_HEIGHT = 20;
+	private static final int EYE_HEIGHT = 7;
 	private static final int EYE_WIDTH = 10;
 	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 75;
+	private static final int MOUTH_HEIGHT = 5;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
@@ -44,7 +44,7 @@ public class Cat {
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("mew", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("purrrrrr", catX, catY+HEAD_DIMENSION+10);	
 		g2.drawString("--Cat", catX+5, catY+HEAD_DIMENSION+20);	
 	}
 }
